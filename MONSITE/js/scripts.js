4f58bdc3f596617ce5409d8ed5f108055264cebf
bagayoko.yan <bagayoko.yan@gmail.com>
2026-04-03 10:46:35 +0200

    commit JS

--- MONSITE/js/scripts.js
+++ MONSITE/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
 * Start Bootstrap - Creative v7.0.7 (https://startbootstrap.com/theme/creative)
 * Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+* Licensed ! under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
 */
 //
 // Scripts
